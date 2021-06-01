e8c42e064346b07ee13e5b90c28ea66194abe792
harld-x <danilyudin723@gmail.com>
2021-06-01 22:21:38 +0700

    2
    useref, useeffect, added links on card and home pages

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,38 +1,5 @@
 import React, { useState } from 'react';
 
-// export class Categories extends Component {
-//   state = {
-//     activeItem: 3,
-//   };
-
-//   onSelectItem = (key) => {
-//     this.setState({
-//       activeItem: key,
-//     });
-//   };
-
-//   render() {
-//     const { items, onClickItem } = this.props;
-//     return (
-//       <div className="categories">
-//         <ul>
-//           <li>Все</li>
-//           {items.map((item, key) => (
-//             <li
-//               className={this.state.activeItem === key ? 'active' : ''}
-//               onClick={() => this.onSelectItem(key)}
-//               key={`${item}_${key}`}>
-//               {item}
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Categories;
-
 const Categories = ({ items, onClickItem }) => {
   const [activeItem, setActiveItem] = useState(null);
 
